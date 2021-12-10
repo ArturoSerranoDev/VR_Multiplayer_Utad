32266d178b75f1ed8bcf2e1e917047ada0e922ff
ArturoInnerspace <arturo.serrano@innerspace.eu>
2021-12-11 00:48:06 +0100

    Added networked interactable

--- Assets/_Project/Scripts/Photon/GameManager.cs
+++ Assets/_Project/Scripts/Photon/GameManager.cs
@@ -14,10 +14,11 @@ namespace Com.MyCompany.MyGame
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
-
         [Tooltip("The prefab to use for representing the player")]
         public GameObject playerPrefab;
-        
+
+        [SerializeField] private GameObject spawnPoint1;
+        [SerializeField] private GameObject spawnPoint2;
         #region Photon Callbacks
 
         private void Start()
@@ -26,7 +27,15 @@ namespace Com.MyCompany.MyGame
             {
                 Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
                 // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
+
+                Vector3 spawnPos = Vector3.zero;
+
+                if (PhotonNetwork.PlayerList.Length <= 1)
+                    spawnPos = spawnPoint1.transform.position;
+                else
+                    spawnPos = spawnPoint2.transform.position;
+                
+                PhotonNetwork.Instantiate(this.playerPrefab.name, spawnPos, Quaternion.identity, 0);
             }
             else
             {
@@ -45,13 +54,10 @@ namespace Com.MyCompany.MyGame
         public override void OnPlayerEnteredRoom(Player other)
         {
             Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
-
-
+            
             if (PhotonNetwork.IsMasterClient)
             {
                 Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
-                
-                LoadArena();
             }
         }
 
@@ -59,20 +65,15 @@ namespace Com.MyCompany.MyGame
         public override void OnPlayerLeftRoom(Player other)
         {
             Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
-
-
+            
             if (PhotonNetwork.IsMasterClient)
             {
                 Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
 
-
-                LoadArena();
             }
         }
-
-
+        
         #endregion
-
 
         #region Public Methods
 
@@ -85,20 +86,6 @@ namespace Com.MyCompany.MyGame
 
         #endregion
         
-        #region Private Methods
-
-
-        void LoadArena()
-        {
-            if (!PhotonNetwork.IsMasterClient)
-            {
-                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
-            }
-            // Debug.LogFormat("PhotonNetwork : Loading Level : {0}");
-            // PhotonNetwork.LoadLevel(1);
-        }
-
-
-        #endregion
+    
     }
 }
